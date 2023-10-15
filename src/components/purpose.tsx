7a7abed477bec4c76c001008dfc4e11e150fbf8b
alfian <devsroot8@gmail.com>
2023-10-15 15:23:31 +0700

    fix(style): remove px mobile view

--- src/components/purpose.tsx
+++ src/components/purpose.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Purpose() {
   return (
-    <section className="w-full bg-zinc-50 py-10 md:py-18 lg:py-40">
-      <div className="max-w-5xl mx-auto px-5 flex items-center gap-5 relative">
+    <section className="w-full bg-gradient-to-b from-zinc-50 to-white py-10 md:py-18 lg:py-40">
+      <div className="max-w-5xl mx-auto flex items-center gap-5 relative">
         <div>
           <img
             src="/icons/defipie-logo-bg.png"
