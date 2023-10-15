7328df8a2338840c1db24252d075bad5cf0c99d7
alfian <devsroot8@gmail.com>
2023-10-15 18:37:17 +0700

    feat(app): mobile nav menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 export default function Header() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <header className="w-full bg-white fixed z-50">
       <nav className="max-w-5xl mx-auto py-4 px-5 md:px-10 flex items-center justify-between">
@@ -23,12 +30,27 @@ export default function Header() {
             Join Now
           </a>
         </div>
-        <button className="md:hidden flex flex-col">
-          <span className="w-8 h-[3px] mb-1 bg-[#8053FF]"></span>
-          <span className="w-8 h-[3px] mb-1 bg-[#8053FF]"></span>
-          <span className="w-8 h-[3px] mb-1 bg-[#8053FF]"></span>
+        <button className="md:hidden flex flex-col" onClick={toggleMenu}>
+          <span className="w-8 h-[3px] mb-[5px] rounded-md bg-[#23155B]"></span>
+          <span className="w-8 h-[3px] mb-[5px] rounded-md bg-[#23155B]"></span>
+          <span className="w-8 h-[3px] mb-[5px] rounded-md bg-[#23155B]"></span>
         </button>
       </nav>
+      {isMenuVisible && (
+        <div className="md:hidden">
+          <div className="w-full min-h-screen flex flex-col gap-20 items-center py-24 px-5 bg-white fixed left-0 ease-in-out duration-300">
+            <a href="" className="text-black">
+              For Projects
+            </a>
+            <a href="" className="text-black">
+              For Investors
+            </a>
+            <a href="" className="py-4 px-10 bg-[#8053FF] rounded-md">
+              Join Now
+            </a>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
